681bc6741b800be88199e5cc6bc2d2a2b8f0df7e
Majrul Ansari <majrul@gmail.com>
2023-07-15 12:57:18 +0530

    initial selenium test case for login module

---
+++ my-shopping-app/src/test/java/com/cdac/test/LoginTest.java
@@ -0,0 +1,17 @@
+package com.cdac.test;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+	@Test
+	public void login_should_work() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost:8181/my-shopping-app/");
+		driver.findElement(By.id("loginLink")).click();
+		driver.close();
+	}
+}
